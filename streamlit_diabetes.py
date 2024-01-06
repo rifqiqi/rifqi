920235ada6027163c47576632c7615a4d853b3d6
rifqiqi <152049023+rifqiqi@users.noreply.github.com>
2024-01-06 13:20:39 +0700

    Update streamlit_diabetes.py

--- streamlit_diabetes.py
+++ streamlit_diabetes.py
@@ -1,5 +1,9 @@
 import pickle
 import streamlit as st
+st.set_page_config(
+    page_title="Cek Kesehatan | Diabetes",
+    page_icon="👨‍🎓",
+)
 
 # membaca model
 diabetes_model = pickle.load(open('diabetes_model.sav','rb'))
